fa3dd956eacad1dd0851d05c8807da0471fa7b81
DhutaAzikira <101611329+DhutaAzikira@users.noreply.github.com>
2022-04-19 16:44:26 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,9 +25,19 @@ namespace Toko
             this.gaji = gaji < 0 ? 0 : gaji;
         }
 
+        public void naik()
+        {
+            int kenaikan;
+
+            kenaikan = gaji * 110;
+            gaji = kenaikan / 100;
+
+            
+        }
+
         static void Main(string[] args)
         {
-            Karyawan a1 = new Karyawan("Paijo", "190302123", 4800000);
+            Karyawan a1 = new Karyawan("Paijo", "190302123", 4000000);
             Karyawan a2 = new Karyawan("Jono", "190302124", 2000000);
 
             Console.WriteLine("No Nik/Nama      Gaji Bulanan");
@@ -35,10 +45,22 @@ namespace Toko
             Console.WriteLine("1."); a1.cetak();
             Console.WriteLine("2."); a2.cetak();
 
+            a1.naik();
+            a2.naik();
+
+            Console.WriteLine("asik naik gaji!");
+
+            Console.WriteLine("No Nik/Nama      Gaji Bulanan");
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("1."); a1.cetak();
+            Console.WriteLine("2."); a2.cetak();
+
+            Console.WriteLine("Klik untuk keluar");
             Console.ReadKey();
         }
     }
 }
+
 
 
 
